26d1b95258baa4ddcd22f9e35525fd116312e8d3
kmac321 <64026121+kmac321@users.noreply.github.com>
2020-11-23 09:17:23 +0000

    Added Card class

---
+++ InClass/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass
+{
+    public class Card
+    {
+        public string CardName { get; set; }
+
+        public string Suit { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{CardName} of {Suit}");
+        }
+    }
+}
